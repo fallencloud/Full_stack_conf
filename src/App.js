3c5b427ba90b4593d270a2b897d42551510c08d6
Sharina V. Jones <sharinavjones@gmail.com>
2019-04-03 18:01:15 -0400

    adding project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import About from "./About";
+import Speakers from "./Speakers";
+import Schedule from "./Schedule";
+import Signup from "./Signup";
+import Footer from "./Footer";
+import Modal from "./Modal";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Navbar />
+        <Jumbotron />
+        <div class="container pt-4 text-left">
+          <About />
+          <h1 id="speakers" class="display-4 text-center my-5 text-muted">
+            Speakers
+          </h1>
+          <Speakers />
+          <h1 id="schedule" class="display-4 text-center my-5 text-muted">
+            Schedule
+          </h1>
+          <Schedule />
+          <button
+            type="button"
+            class="btn btn-outline-info btn-lg d-block mx-auto my-5"
+            data-toggle="modal"
+            data-target="#register"
           >
-            Learn React
-          </a>
-        </header>
+            Don't Miss Out, Register Now
+          </button>
+          <hr />
+          <Signup />
+          <hr />
+          <Footer />
+        </div>
+        <Modal />
       </div>
     );
   }
